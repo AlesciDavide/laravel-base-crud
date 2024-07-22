8bb48738e079ac3a9cdea7b7c63e33550d7514da
AlesciDavide <davi.alesci@Gmail.com>
2024-07-22 13:48:45 +0200

    milestone 1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AnimalController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('pages.home');
-});
+Route::get('/', [AnimalController::Class, 'index'])->name('pages.index');
+
+
+
+
+/* Route::resource('Animal', AnimalController::class); */
